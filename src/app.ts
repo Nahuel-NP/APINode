480b9b49f8349c4be1005f59cc47df727fbac3cb
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-01-01 16:55:34 -0300

    create envs config for env

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
+import { envs } from "./config/envs";
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
 
 
 function main() {
   const server = new Server({
-    port: 3000,
+    port: envs.PORT ,
     routes: AppRoutes.routes
   })
 
