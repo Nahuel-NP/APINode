9c80bc6d58dad6cabe6c54b43f63363897cef1c6
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-03 16:44:00 -0300

    create user entity

---
+++ src/domain/entities/user.entity.ts
@@ -0,0 +1,19 @@
+export class UserEntity {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly email: string,
+    public readonly password: string,
+  ) {}
+
+  static fromObject(object: { [key: string]: any }): UserEntity {
+    const { id, name, email, password } = object;
+
+    if (!id) throw new Error('Id is required');
+    if (!name) throw new Error('Name is required');
+    if (!email) throw new Error('Email is required');
+    if (!password) throw new Error('Password is required');
+
+    return new UserEntity(id, name, email, password);
+  }
+}
