d579d92a1a376084a496c6fef70546b7a92f8069
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-04 21:01:00 -0300

    implement register user

--- src/presentation/auth/auth.service.ts
+++ src/presentation/auth/auth.service.ts
@@ -1,10 +1,45 @@
+import { bcryptAdapter } from '../../config/bcrypt.adapter';
+import { prisma } from '../../config/prismaClient';
 import { LoginUserDto } from '../../domain/dtos/auth/login-user.dto';
+import { RegisterUserDto } from '../../domain/dtos/auth/register-user.dto';
+import { UserEntity } from '../../domain/entities/user.entity';
+import { CustomError } from '../../domain/errors/custom.error';
 
 export class AuthService {
-  constructor() /* Inject */
-  {}
+  constructor /* Inject */() {}
 
   public async loginUser(loginUserDto: LoginUserDto) {
     return loginUserDto;
   }
+
+  public async registerUser(registerUserDto: RegisterUserDto) {
+    const existUser = await prisma.user.findFirst({
+      where: {
+        email: registerUserDto.email,
+      },
+    });
+    
+    if (existUser) throw CustomError.badRequest('User already exists');
+    try {
+
+      const newUser = await prisma.user.create({
+        data: {
+          ...registerUserDto,
+          password: bcryptAdapter.hash(registerUserDto.password),
+        },
+      });
+
+      const { password: _, ...user } = UserEntity.fromObject(newUser);
+      return {
+        user,
+      };
+    } catch (error) {
+      console.log(error);
+      throw CustomError.internalServer(`${error}`);
+    }
+  }
+
+  private sendValidationEmail = async () => {
+    //TODO implement
+  };
 }
