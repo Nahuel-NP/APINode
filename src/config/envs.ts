f447cce9be816cb01b9989954f8fb6072ba5ffed
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-04 21:32:19 -0300

    adding env to envs file

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,6 +1,12 @@
-import "dotenv/config";
-import { get } from "env-var";
+import 'dotenv/config';
+import { get } from 'env-var';
 
 export const envs = {
-  PORT: get("PORT").required().asPortNumber(),
+  PORT: get('PORT').required().asPortNumber(),
+  DATABASE_URL: get('DATABASE_URL').required().asString(),
+  POSTGRES_USER: get('POSTGRES_USER').required().asString(),
+  POSTGRES_DB: get('POSTGRES_DB').required().asString(),
+  POSTGRES_PORT: get('POSTGRES_PORT').required().asPortNumber(),
+  POSTGRES_PASSWORD: get('POSTGRES_PASSWORD').required().asString(),
+  JWT_SEED: get('JWT_SEED').required().asString(),
 };
