a02dbc34ccb34d8322379a754803ee9cc7c7c93e
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-05 15:47:38 -0300

    config env for testing

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,12 @@
 /// <reference types="vitest/globals"/>
 import { defineConfig } from 'vitest/config';
+import dotenv from 'dotenv'
+
+if (process.env.VITEST) {
+  dotenv.config({ path: '.env.test' });
+} else {
+  dotenv.config({ path: '.env' });
+}
 
 export default defineConfig({
   test: {
