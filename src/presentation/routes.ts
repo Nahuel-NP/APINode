f177b4088af701ae4a63303685473a3d9b5d8d72
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-06 22:29:01 -0300

    adding display routes

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { AuthRoutes } from "./auth/auth.routes";
+import { DisplayRoutes } from "./display/display.routes";
 
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
     router.use('/api/auth', AuthRoutes.routes);
+    router.use('/api/display', DisplayRoutes.routes);
     return router;
   }
 }
