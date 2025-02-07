6778b23caac4e89e40747116ff596e773d89eb49
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-06 22:08:05 -0300

    create display entity

---
+++ src/domain/entities/display.entity.ts
@@ -0,0 +1,29 @@
+export class DisplayEntity {
+  constructor(
+    public readonly id: string,
+    public readonly name: string,
+    public readonly description: string,
+    public picture_ur: string,
+    public readonly user_id: number,
+    public readonly price_per_day: string,
+    public readonly resolution_height: string,
+    public readonly resolution_width: string,
+    public readonly type: string,
+  ) {}
+
+  static fromObject(object: { [key: string]: any }): DisplayEntity {
+    const { id, name, description, picture_ur, user_id, price_per_day, resolution_height, resolution_width, type } = object;
+
+    if (!id) throw new Error('Id is required');
+    if (!name) throw new Error('Name is required');
+    if (!description) throw new Error('Description is required');
+    if (!picture_ur) throw new Error('Picture is required');
+    if (!user_id) throw new Error('User id is required');
+    if (!price_per_day) throw new Error('Price per day is required');
+    if (!resolution_height) throw new Error('Resolution height is required');
+    if (!resolution_width) throw new Error('Resolution width is required');
+    if (!type) throw new Error('Type is required');
+
+    return new DisplayEntity(id, name, description, picture_ur, user_id, price_per_day, resolution_height, resolution_width, type);
+  }
+}
