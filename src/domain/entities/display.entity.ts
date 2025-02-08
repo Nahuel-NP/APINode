551421c10dff6eb7fb10b0e004025070e96184a0
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-08 18:21:27 -0300

    fix picture_url name property

--- src/domain/entities/display.entity.ts
+++ src/domain/entities/display.entity.ts
@@ -3,7 +3,7 @@ export class DisplayEntity {
     public readonly id: number,
     public readonly name: string,
     public readonly description: string,
-    public picture_ur: string,
+    public picture_url: string,
     public readonly user_id: string,
     public readonly price_per_day: string,
     public readonly resolution_height: string,
@@ -12,18 +12,18 @@ export class DisplayEntity {
   ) {}
 
   static fromObject(object: { [key: string]: any }): DisplayEntity {
-    const { id, name, description, picture_ur, user_id, price_per_day, resolution_height, resolution_width, type } = object;
+    const { id, name, description, picture_url, user_id, price_per_day, resolution_height, resolution_width, type } = object;
 
     if (!id) throw new Error('Id is required');
     if (!name) throw new Error('Name is required');
     if (!description) throw new Error('Description is required');
-    if (!picture_ur) throw new Error('Picture is required');
+    if (!picture_url) throw new Error('Picture is required');
     if (!user_id) throw new Error('User id is required');
     if (!price_per_day) throw new Error('Price per day is required');
     if (!resolution_height) throw new Error('Resolution height is required');
     if (!resolution_width) throw new Error('Resolution width is required');
     if (!type) throw new Error('Type is required');
 
-    return new DisplayEntity(id, name, description, picture_ur, user_id, price_per_day, resolution_height, resolution_width, type);
+    return new DisplayEntity(id, name, description, picture_url, user_id, price_per_day, resolution_height, resolution_width, type);
   }
 }
