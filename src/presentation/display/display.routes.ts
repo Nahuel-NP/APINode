4a656c70a88cecb26636c1b6536a8150f5ec1227
Nahuel-NP <pedroso.nahuel.dev@gmail.com>
2025-02-07 22:25:26 -0300

    use auth middleware

--- src/presentation/display/display.routes.ts
+++ src/presentation/display/display.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { DisplayController } from './display.controller';
 import { DisplayService } from './display.service';
+import { AuthMiddlaware } from '../middlewares/auth.middleware';
 
 export class DisplayRoutes {
   static get routes(): Router {
     const router = Router();
     const displayService = new DisplayService();
     const displayController = new DisplayController(displayService);
-    router.get('/', displayController.getAllDisplays);
-    router.get('/:id', displayController.getDisplayById);
+    router.get('/',[AuthMiddlaware.validateJWT], displayController.getAllDisplays);
+    router.get('/:id',[AuthMiddlaware.validateJWT], displayController.getDisplayById);
     return router;
   }
 }
